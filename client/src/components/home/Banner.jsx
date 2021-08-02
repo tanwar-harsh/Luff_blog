fa7eae043a39a9832f1433138cb38a5b98c26676
luffyguy <harshtanwar25@gmail.com>
2021-08-02 14:09:01 +0530

    added css

--- client/src/components/home/Banner.jsx
+++ client/src/components/home/Banner.jsx
@@ -1,12 +1,25 @@
 import { makeStyles, Box, Typography } from "@material-ui/core";
 
-const url = "https://i.imgur.com/uaPwCQE.jpg";
+const url1 = "https://i.imgur.com/o1Dc1Uj.png";
 
 const useStyles = makeStyles({
   image: {
-    background: `url(${url}) center/55% repeat-x #000`,
-    width: '100vw',
-    height: '50vh',
+    background: `url(${url1}) center/55% repeat-x #000`,
+    width: "100vw",
+    height: "50vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    "& :first-child": {
+      fontSize: 50,
+        color: "white",
+      lineHeight: 1,
+    },
+    "& :last-child": {
+      fontSize: 70,
+      color: "white",
+    },
   },
 });
 
@@ -14,7 +27,7 @@ const Banner = () => {
   const classes = useStyles();
   return (
     <Box className={classes.image}>
-      <Typography>Welcome</Typography>
+      <Typography>Welcome to,</Typography>
       <Typography>Luff-Blog</Typography>
     </Box>
   );
