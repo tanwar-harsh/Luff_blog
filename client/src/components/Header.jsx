09da7b427f383a3b23ba188ce04ea214289890d8
luffyguy <harshtanwar25@gmail.com>
2021-08-02 14:08:33 +0530

    fixed header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,26 +2,26 @@ import { AppBar, Toolbar, Typography, makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles({
   component: {
-    background: 'white',
-    color: 'black',
+    background: "white",
+    color: "black",
   },
   container: {
-    justifyContent: 'center',
-    '& > *': {
+    justifyContent: "center",
+    "& > *": {
       padding: 20,
     },
-  }
-})
+  },
+});
 
 const Header = () => {
   const classes = useStyles();
   return (
-    <AppBar className={ classes.component}>
-      <Toolbar className={ classes.container}>
-              <Typography>HOME</Typography>
-              <Typography>ABOUT</Typography>
-              <Typography>CONTACT</Typography>
-              <Typography>LOGIN</Typography>
+    <AppBar className={classes.component}>
+      <Toolbar className={classes.container}>
+        <Typography>HOME</Typography>
+        <Typography>ABOUT</Typography>
+        <Typography>CONTACT</Typography>
+        <Typography>LOGIN</Typography>
       </Toolbar>
     </AppBar>
   );
