641692b31dfd3904ad0960eecdfcabe77132c145
luffyguy <harshtanwar25@gmail.com>
2021-08-03 18:32:50 +0530

    Updated Posts

--- client/src/components/home/Posts.jsx
+++ client/src/components/home/Posts.jsx
@@ -1,4 +1,5 @@
 import { Grid } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 //components
 import Post from "./Post";
@@ -6,8 +7,10 @@ import Post from "./Post";
 const Posts = () => {
   let posts = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   return posts.map((post) => (
-    <Grid item lg={3} sm={4} xs={12}>
-      <Post />
+    <Grid item lg={3} md={4} sm={12} xs={12}>
+      <Link to="/detail" style={{ textDecoration: "none", color: "inherit" }}>
+        <Post />
+      </Link>
     </Grid>
   ));
 };
