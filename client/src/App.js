cf83d6f45c2c65bbcf062208d726aed4b90710d4
luffyguy <harshtanwar25@gmail.com>
2021-08-03 18:33:54 +0530

    Created Update page

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,27 @@
 import React from "react";
 import { Box } from "@material-ui/core";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 //components
 import Header from "./components/Header";
 import Home from "./components/home/Home";
+import DetailView from "./components/post/DetailView";
+import CreateView from "./components/post/CreateView";
+import UpdateView from "./components/post/UpdateView";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Box style={{ marginTop: "64px" }}>
-        <Home />
+      <Box style={{ marginTop: 64 }}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/detail" component={DetailView} />
+          <Route path="/create" component={CreateView} />
+          <Route path="/update" component={UpdateView} />
+        </Switch>
       </Box>
-    </>
+    </BrowserRouter>
   );
 }
 
